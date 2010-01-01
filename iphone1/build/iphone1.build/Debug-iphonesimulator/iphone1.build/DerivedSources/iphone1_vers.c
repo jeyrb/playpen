5434134a2df6e005a9c322f044f4d5025e558377
jey burrows <jrb@jeymail.net>
2010-01-01 17:09:54 +0000

    remove build dir

--- iphone1/build/iphone1.build/Debug-iphonesimulator/iphone1.build/DerivedSources/iphone1_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char iphone1VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:iphone1  PROJECT:iphone1-0.2" "\n"; const double iphone1VersionNumber __attribute__ ((used)) = (double)0.2;
