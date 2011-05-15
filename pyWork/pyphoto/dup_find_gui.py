7d97274114ee2ee9092d5f3964e58246f6fd4912
jey burrows <jrb@jeymail.net>
2011-05-15 18:13:45 +0100

    close of sun 15

--- pyWork/pyphoto/dup_find_gui.py
+++ pyWork/pyphoto/dup_find_gui.py
@@ -5,6 +5,7 @@ import appscript
 import sys
 
 from pyphoto.dup_find import ProgressReporter,DupFinder
+import wx.grid.PyGridTableBase
 
 class DupFinderFrame(wx.Frame):
     rowlimit=0
@@ -15,7 +16,7 @@ class DupFinderFrame(wx.Frame):
         self._define_toolbar()
         self._define_grid(panel)
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.grid, 1, wx.EXPAND)
+        sizer.Add(self._grid, 1, wx.EXPAND)
         panel.SetSizer(sizer)
         self.CreateStatusBar()
         self._define_menu()
@@ -43,27 +44,16 @@ class DupFinderFrame(wx.Frame):
 
 
     def _define_grid(self, panel):
-        self.grid = wx.grid.Grid(panel)
-        self.grid.CreateGrid(self.rowlimit, 6)
-        self.grid.EnableEditing(False)
+        self._grid = wx.grid.Grid(panel)
+        self._grid_model = DupFinderGridModel()
+        self._grid.SetTable(self._grid_model)
+        self._grid.CreateGrid(self.rowlimit, 6)
+        self._grid.EnableEditing(False)
         #self.grid.AutoSizeColumns(True)
         #dup_colour = wx.Colour(255,239,213)
         #prior_colour = wx.Colour(238,210,238)
-        self.grid.SetRowLabelSize(0)
-        self.grid.SetLabelBackgroundColour('MIDNIGHT BLUE')
-        self.grid.SetLabelTextColour('WHITE')
-        self.grid.SetColLabelValue(0, "A ID")
-        self.grid.SetColLabelValue(1, "A Name")
-        self.grid.SetColSize(1,150)
-        self.grid.SetColLabelValue(2, "A Path")
-        self.grid.SetColSize(2,200)
-        self.grid.SetColLabelValue(3, "B ID")
-        self.grid.SetColLabelValue(4, "B Name")
-        self.grid.SetColSize(4,150)
-        self.grid.SetColLabelValue(5, "B Path")
-        self.grid.SetColSize(5,200)
 
-        self.grid.Bind(wx.grid.EVT_GRID_CELL_RIGHT_CLICK,
+        self._grid.Bind(wx.grid.EVT_GRID_CELL_RIGHT_CLICK,
                        self.OnRightClickGrid)
 
     def OnRightClickGrid(self,event):
@@ -89,8 +79,7 @@ class DupFinderFrame(wx.Frame):
             id_col = 0
         else:
             id_col = 3
-        id = self.grid.GetCellValue(self.cicked_row,id_col)
-        print "MENU: ID %s PHOTO %s" % (event.GetId(),id)
+        id = self._grid.GetCellValue(self.cicked_row,id_col)
         self.iPhoto.photo_library_album().photos.ID(int(id)).select()
         self.iPhoto.reopen()
 
@@ -108,6 +97,8 @@ class DupFinderFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnScan, menuScan)
         menuClean = filemenu.Append(9002, "&Clean", "Clean out the duplicate scanning cache")
         self.Bind(wx.EVT_MENU, self.OnClean, menuClean)
+        menuExport = filemenu.Append(9903,"E&xport","Export results to a text file")
+        self.Bind(wx.EVT_MENU, self.OnExport,menuExport)
         self.SetMenuBar(menuBar)
 
     def OnClean(self,event):
@@ -118,6 +109,9 @@ class DupFinderFrame(wx.Frame):
         dlg = wx.MessageDialog(self, "A utility to detect duplicate photos in iPhoto", "About iPhotoDupFind", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
+
+    def OnExport(self,event):
+        pass
 
     def OnExit(self, event):
         print "Shutting down Dup Finder GUI"
@@ -134,9 +128,9 @@ class DupFinderFrame(wx.Frame):
 
     def OnScan(self, event):
         self.Report("Starting scan of library ...")
-        self.grid.ClearGrid()
-        if self.grid.NumberRows>0:
-            self.grid.DeleteRows(0,self.grid.NumberRows)
+        self._grid.ClearGrid()
+        if self._grid.NumberRows>0:
+            self._grid.DeleteRows(0,self._grid.NumberRows)
         wx.Yield()
         row = 0
 
@@ -144,20 +138,34 @@ class DupFinderFrame(wx.Frame):
         try:
             for (dup, prior) in self._df.find_dups():
                 if row >= self.rowlimit:
-                    self.grid.AppendRows(1)
+                    self._grid.AppendRows(1)
                     self.rowlimit=self.rowlimit+1
-                self.grid.SetCellValue(row,0,str(dup.id))
-                self.grid.SetCellValue(row,1,dup.name)
-                self.grid.SetCellValue(row,2,dup.image_path)
-                self.grid.SetCellValue(row,3,str(prior.id))
-                self.grid.SetCellValue(row,4,prior.name)
-                self.grid.SetCellValue(row,5,prior.image_path)
+                self._grid.SetCellValue(row,0,str(dup.id))
+                self._grid.SetCellValue(row,1,dup.name)
+                self._grid.SetCellValue(row,2,dup.image_path)
+                self._grid.SetCellValue(row,3,str(prior.id))
+                self._grid.SetCellValue(row,4,prior.name)
+                self._grid.SetCellValue(row,5,prior.image_path)
                 wx.Yield()
                 row = row + 1
         except:
             self.Report("Library scan aborted (%s)" % sys.exc_info()[1])
         self.Report("Scan completed")
 
+class DupFinderGridModel(wx.grid.PyGridTableBase):
+        self.base_SetRowLabelSize(0)
+        self.base_SetLabelBackgroundColour('MIDNIGHT BLUE')
+        self._grid.SetLabelTextColour('WHITE')
+        self._grid.SetColLabelValue(0, "A ID")
+        self._grid.SetColLabelValue(1, "A Name")
+        self._grid.SetColSize(1,150)
+        self._grid.SetColLabelValue(2, "A Path")
+        self._grid.SetColSize(2,200)
+        self._grid.SetColLabelValue(3, "B ID")
+        self._grid.SetColLabelValue(4, "B Name")
+        self._grid.SetColSize(4,150)
+        self._grid.SetColLabelValue(5, "B Path")
+        self._grid.SetColSize(5,200)
 
 class DupFinderGUI(object):
     def __init__(self):
